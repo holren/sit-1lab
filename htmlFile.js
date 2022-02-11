78c1e81247f0f3f6802dfe7a630901648f41fd76
holren <katya_ns@mail.ru>
2022-02-11 16:48:20 +0300

    third part server code (added htmlFile)

---
+++ htmlFile.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require('fs').promises;
+
+const requestListener = function (req, res) {
+	fs.readFile(__dirname + "/index.html")
+		.then(contents => {
+			res.setHeader("Content-Type", "text/html");
+			res.writeHead(200);
+			res.end(contents);
+		}
+	)
+};
